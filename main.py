fa65e71314ab1a4ae195931423218a9d9894c94c
Michał Toczek <toczekmj@gmail.com>
2023-10-08 19:04:44 +0200

    test gpg key

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def print_hi(name):
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    print(f'Hi, {name}, from test branch')  # Press ⌘F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
+#testgpg
 
